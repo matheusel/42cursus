54474a575920fbcaa78a6cda996acaa9c797133d
mc-m-el- <matheuselh@gmail.com>
2024-02-08 17:42:18 -0300

    ok

--- minitalk/server.c
+++ minitalk/server.c
@@ -1,8 +1,8 @@
 #include "minitalk.h"
 
-void	handler()
+void	handler(int	sig)
 {
-
+	ft_printf("handler");
 }
 
 int	main(void)
@@ -10,10 +10,23 @@ int	main(void)
 	int	pid;
 	struct	sigaction	sa;
 
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_handler = handler;
+
 	ft_printf("PID: %i\n", pid = getpid());
 	if (pid < 0)
 	{
 		ft_printf("\nErro: Não foi possível obter o PID\n");
 		return -1;
+	}
+	ft_printf("\nAguardando por uma mensagem...\n");
+	while (1)
+	{	
+		if (sigaction(SIGUSR1, &sa, NULL) < 0)
+			ft_printf("\nErro: Não foi possível obter o PID\n");
+		else if (sigaction(SIGUSR2, &sa, NULL) < 0)
+			ft_printf("\nErro: Não foi possível obter o PID\n");
+		pause();
 	}
 }
