22b9adb3ef172c466e440342c279044fceae4e07
matheusel <matheuselh@gmail.com>
2024-03-28 16:57:35 -0300

    fdf added printf

---
+++ fdf/fdf.c
@@ -0,0 +1,16 @@
+#include "fdf.h"
+
+/*int valid_parms()
+{
+
+}*/
+
+int main(int argc, char *argv[])
+{
+	if(argc == 2)
+	{
+
+	}
+	ft_printf("Error! Using: ./fdf ARCHIVE");
+	return NULL;
+}
---
+++ fdf/fdf.h
@@ -0,0 +1,6 @@
+#ifndef FDF_H
+# define FDF_H
+
+#include "printf/ft_printf.h"
+
+#endif
